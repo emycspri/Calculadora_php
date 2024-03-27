66e79d94ece5dea75b4e4e0fe1bb8d3fbe1c1475
emycspri <127248691+emycspri@users.noreply.github.com>
2024-03-27 16:11:34 -0300

    Add files via upload

---
+++ funciona/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+</head>
+<body>
+   
+<form action="" method="post" >
+      Primeiro Numero: <input name="num1" type="text"><br>
+      Segundo numero: <input name="num2" type="text"><br>
+      <input type="submit" name="operacao" value="+">     
+      <input type="submit" name="operacao" value="-">     
+      <input type="submit" name="operacao" value="*">     
+      <input type="submit" name="operacao" value="/">     
+   </form> 
+   <br>
+<?php
+
+   $a = $_POST['num1'];
+   $b = $_POST['num2'];
+   $op= $_POST['operacao'];
+
+   if( !empty($op) ) {
+      if($op == '+')
+         $c = $a + $b;
+      else if($op == '-')
+         $c = $a - $b;
+      else if($op == '*')
+         $c = $a*$b;
+      else
+         $c = $a/$b;
+
+      echo "O resultado da opera&ccedil;&atilde;o &eacute;: $c";
+   }
+?>
+</body>
+</html>
